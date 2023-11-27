ddb4968bd957cedadac032c777f1ecb4243ac55c
ItsOnlyBinary <git@itsonlybinary.com>
2023-11-27 13:48:31 +0000

    update dependencies

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,12 +4,10 @@ module.exports = {
         'alpha-value-notation': null,
         'color-function-notation': null,
         'declaration-no-important': true,
-        'indentation': 4,
         'no-descending-specificity': null,
         'no-empty-first-line': null,
         'property-no-vendor-prefix': null,
         'selector-class-pattern': null,
         'shorthand-property-no-redundant-values': null,
-        'string-quotes': 'single',
     },
 };
